e35272d53c1edf7eb64e18e8369a4bc0d9cb146b
alyoook3000 <yoo84@purdue.edu>
2022-06-05 12:26:31 +0900

    Initial commit

--- Pythagoreantriplet.py
+++ Pythagoreantriplet.py
@@ -1,2 +1,28 @@
-def tripletproduct():
-    
+import math
+def tripletproduct(sidesum):
+
+#Euclid's formula:
+
+#a = m**2 - n**2
+#b = 2mn
+#c = m**2 + n**2
+#m > n > 0
+
+#Derived formula
+#a+b+c = 2m**2 + 2mn = 2m(m+n)
+
+#2 * m * (m+n) = sidesum
+#n = (sidesum / (2*m))-m
+
+    for m in range(1, round(math.sqrt(sidesum))):
+        n = (sidesum / (2*m))-m
+        # m have to be larger than n
+        if n < 0 or n>= m:
+            continue
+        if 2 * m * (m+round(n)) == sidesum:
+            return (m**2-n**2, 2*m*n, m**2 + n**2)
+
+
+
+a, b, c = tripletproduct(1000)
+print(a, b, c, a*b*c)
