3582abf40d6c46898f01841a7643f5e04c23a9d3
alyoook3000 <yoo84@purdue.edu>
2022-06-05 20:31:41 +0900

    Challenge 10

--- SummationofPrimes.py
+++ SummationofPrimes.py
@@ -3,6 +3,7 @@ def prime_sum_sieve(limit):
     is_prime = [True for i in nums]
     is_prime[0] = False
     list_prime = []
+    sumPrimes = sum(list_prime)
 
 
     nums = [i for i in range(1, limit+1)]
@@ -18,8 +19,6 @@ def prime_sum_sieve(limit):
     for i in range(2, len(prime_array)):
         if is_prime[i] == True:
             list_prime.append(nums[i])
-
-    sumPrimes = sum(list_prime)
 
     return sumPrimes
 
